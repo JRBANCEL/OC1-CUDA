e974ec5e03f7d86ade5200f4f9afcd77f79a8408
Jean-Rémy Bancel <bancel@seas.harvard.edu>
2012-12-01 01:41:19 -0500

    Working (close to...)

--- serialOC1.py
+++ serialOC1.py
@@ -1,3 +1,5 @@
+import math
+import matplotlib.pyplot as plt
 import numpy as np
 import random
 from kNN import Classifier
@@ -50,35 +52,77 @@ class SerialOC1(Classifier):
         R = 0
         coeff = np.zeros(d + 1)
         Pmove = Pstag
+        result = list()
         # Main Loop
         while R < 5:
+            print "R", R
             # Choose a random hyperplan
             if R == 0:
                 coeff[featureMin] = 1
                 coeff[d] = -splitMin
             else:
-                pass
+                coeff = np.random.uniform(0, 1, d+1)
 
-            # Perturb coefficients in sequence
-            for m in range(d):
-                # Compute U_j
-                U = np.zeros(n)
-                for j in range(n):
-                    V = coeff[-1] + sum([coeff[i]*sample[j][i] for i in range(d)])
-                    U[j] = (coeff[m]*sample[j][m] - V)/float(sample[j][m])
-                Usorted = U.copy()
-                Usorted.sort()
-                # Find best univariate split for sorted U_j
-                impurityMin = 100000
-                splitMin = 0
-                for split in [(Usorted[i]+Usorted[i+1])/2.\
-                              for i in range(n-1)]:
-                    # Compute impurity
+            nextRound = False
+            J = 5
+            while not nextRound:
+                # Perturb coefficients in sequence
+                for m in range(d):
+                    # Compute U_j
+                    U = np.zeros(n)
+                    for j in range(n):
+                        V = coeff[-1] + sum([coeff[i]*sample[j][i] for i in range(d)])
+                        U[j] = (coeff[m]*sample[j][m] - V)/float(sample[j][m])
+                    Usorted = U.copy()
+                    Usorted.sort()
+                    # Find best univariate split for sorted U_j
+                    impurityMin = 100000
+                    splitMin = 0
+                    for split in [(Usorted[i]+Usorted[i+1])/2.\
+                                  for i in range(n-1)]:
+                        # Compute impurity
+                        Tl = Tr = 0
+                        Lr = np.zeros(n)
+                        Ll = np.zeros(n)
+                        for index, point in enumerate(U):
+                            if point < split:
+                                Tl += 1
+                                Ll[cat[index]] += 1
+                            else:
+                                Tr += 1
+                                Lr[cat[index]] += 1
+                        GiniL = 1 - sum([(l/float(Tl))**2 for l in Ll])
+                        GiniR = 1 - sum([(r/float(Tr))**2 for r in Lr])
+                        impurity = (Tl*GiniL + Tr*GiniR)/float(n)
+                        if impurity < impurityMin:
+                            impurityMin = impurity
+                            splitMin = split
+                    H1 = coeff.copy()
+                    H1[m] = splitMin
+                    print coeff, H1
+
+                    # Compare Impurity
                     Tl = Tr = 0
                     Lr = np.zeros(n)
                     Ll = np.zeros(n)
-                    for index, point in enumerate(U):
-                        if point < split:
+                    for index in range(n):
+#                        print "Point: %d, Value: %f" % (index, sum([coeff[i]*sample[index][i] for i in range(d)]) + coeff[d])
+                        if sum([coeff[i]*sample[index][i] for i in range(d)]) + coeff[d] > 0:
+                            Tl += 1
+                            Ll[cat[index]] += 1
+                        else:
+                            Tr += 1
+                            Lr[cat[index]] += 1
+#                        print Tl, Tr
+                    GiniL = 1 - sum([(l/float(Tl))**2 for l in Ll])
+                    GiniR = 1 - sum([(r/float(Tr))**2 for r in Lr])
+                    impurityH = (Tl*GiniL + Tr*GiniR)/float(n)
+
+                    Tl = Tr = 0
+                    Lr = np.zeros(n)
+                    Ll = np.zeros(n)
+                    for index in range(n):
+                        if sum([H1[i]*sample[index][i] for i in range(d)]) + H1[d] > 0:
                             Tl += 1
                             Ll[cat[index]] += 1
                         else:
@@ -86,57 +130,104 @@ class SerialOC1(Classifier):
                             Lr[cat[index]] += 1
                     GiniL = 1 - sum([(l/float(Tl))**2 for l in Ll])
                     GiniR = 1 - sum([(r/float(Tr))**2 for r in Lr])
-                    impurity = (Tl*GiniL + Tr*GiniR)/float(n)
-                    if impurity < impurityMin:
-                        impurityMin = impurity
-                        splitMin = split
-                H1 = coeff.copy()
-                H1[m] = splitMin
-                print coeff, H1
-
-                # Compare Impurity
-                Tl = Tr = 0
-                Lr = np.zeros(n)
-                Ll = np.zeros(n)
-                for index in range(n):
-#                    print "Point: %d, Value: %f" % (index, sum([coeff[i]*sample[index][i] for i in range(d)]) + coeff[d])
-                    if sum([coeff[i]*sample[index][i] for i in range(d)]) + coeff[d] > 0:
-                        Tl += 1
-                        Ll[cat[index]] += 1
-                    else:
-                        Tr += 1
-                        Lr[cat[index]] += 1
-#                    print Tl, Tr
-                GiniL = 1 - sum([(l/float(Tl))**2 for l in Ll])
-                GiniR = 1 - sum([(r/float(Tr))**2 for r in Lr])
-                impurityH = (Tl*GiniL + Tr*GiniR)/float(n)
-
-                Tl = Tr = 0
-                Lr = np.zeros(n)
-                Ll = np.zeros(n)
-                for index in range(n):
-                    if H1[m] > U[index]:
-                        Tl += 1
-                        Ll[cat[index]] += 1
-                    else:
-                        Tr += 1
-                        Lr[cat[index]] += 1
-                GiniL = 1 - sum([(l/float(Tl))**2 for l in Ll])
-                GiniR = 1 - sum([(r/float(Tr))**2 for r in Lr])
-                impurityH1 = (Tl*GiniL + Tr*GiniR)/float(n)
-                print "H %f, H1 %f" % (impurityH, impurityH1)
-                if impurityH1 < impurityH:
-                    coeff = H1
-                    Pmove = Pstag
-                elif impurityH1 ==  impurityH:
-                    if random.random() < Pmove:
+                    impurityH1 = (Tl*GiniL + Tr*GiniR)/float(n)
+                    print "H %f, H1 %f" % (impurityH, impurityH1)
+                    if impurityH1 < impurityH:
                         coeff = H1
-                    Pmove -= 0.1*Pstag
-                else:
-                    break
-                print m
+                        Pmove = Pstag
+                    elif impurityH1 ==  impurityH:
+                        if random.random() < Pmove:
+                            coeff = H1
+                        Pmove -= 0.1*Pstag
+                    else:
+                        break
 
+                # Step 2
+                if J < 5:
+                    print "J", J
+                    # Choose a random direction
+                    # See the paper to understant why choosing independently each
+                    # component is bad.
+                    direction = np.random.uniform(0, 1, d+1)
+
+                    # Pertub H in that direction
+                    H1 = coeff + direction
+
+                    # Find alpha such that purity is minimized
+                    alpha = np.zeros(d)
+                    for m in range(d):
+                        # Compute U_j
+                        U = np.zeros(n)
+                        for j in range(n):
+                            V = H1[-1] + sum([H1[i]*sample[j][i] for i in range(d)])
+                            U[j] = (H1[m]*sample[j][m] - V)/float(sample[j][m])
+                        Usorted = U.copy()
+                        Usorted.sort()
+                        # Find best univariate split for sorted U_j
+                        impurityMin = 100000
+                        splitMin = 0
+                        for split in [(Usorted[i]+Usorted[i+1])/2.\
+                                      for i in range(n-1)]:
+                            # Compute impurity
+                            Tl = Tr = 0
+                            Lr = np.zeros(n)
+                            Ll = np.zeros(n)
+                            for index, point in enumerate(U):
+                                if point < split:
+                                    Tl += 1
+                                    Ll[cat[index]] += 1
+                                else:
+                                    Tr += 1
+                                    Lr[cat[index]] += 1
+                            GiniL = 1 - sum([(l/float(Tl))**2 for l in Ll])
+                            GiniR = 1 - sum([(r/float(Tr))**2 for r in Lr])
+                            impurity = (Tl*GiniL + Tr*GiniR)/float(n)
+                            if impurity < impurityMin:
+                                impurityMin = impurity
+                                splitMin = split
+                        alpha[m] = (splitMin - coeff[m])/direction[m]
+                        H1[m] = splitMin
+                    print "Alpha:", alpha[-1]
+                    H1 = coeff + alpha[-1]*direction
+
+                    # Check if it decreases impurity
+                    Tl = Tr = 0
+                    Lr = np.zeros(n)
+                    Ll = np.zeros(n)
+                    for index in range(n):
+                        if sum([H1[i]*sample[index][i] for i in range(d)]) + H1[d] > 0:
+                            Tl += 1
+                            Ll[cat[index]] += 1
+                        else:
+                            Tr += 1
+                            Lr[cat[index]] += 1
+                    GiniL = 1 - sum([(l/float(Tl))**2 for l in Ll])
+                    GiniR = 1 - sum([(r/float(Tr))**2 for r in Lr])
+                    impurityH1 = (Tl*GiniL + Tr*GiniR)/float(n)
+                    print "H %f, H1 %f" % (impurityH, impurityH1)
+                    if impurityH1 > impurityH:
+                        coeff = H1
+                        nextRound = True
+                    J += 1
+                else:
+                     break
+            result.append((impurityH, coeff))
             R += 1
+        result.sort(key=lambda x: x[0])
+        print result[0]
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        for index, line in enumerate(sample):
+            if cat[index] == 1:
+                ax.plot(line[0], line[1], "ro")
+            elif cat[index] == 2:
+                ax.plot(line[0], line[1], "go")
+            else:
+                ax.plot(line[0], line[1], "bo")
+        f = lambda x: (-result[0][1][0]*x -result[0][1][2])/result[0][1][1]
+        x = np.linspace(0, 4)
+        ax.plot(x, f(x))
+        plt.show()
 
 
 
@@ -155,6 +246,10 @@ if __name__ == "__main__":
                       [2, 2.1, 1],
                       [3, 1.9, 1],
                       [1, 4, 1],
+                      [0.73, 4, 2],
+                      [1, -1, 3],
+                      [1, 1, 3],
+                      [1, 2, 3],
                       ])
     classifier = SerialOC1()
     classifier.trainClassifier(sample)
