20558b7f954d5a43704ad66baf9addc5f40f76dd
Jean-Rémy Bancel <bancel@seas.harvard.edu>
2012-12-09 23:57:29 -0500

    Adding function to compute set impurity

---
+++ set_impurity.c
@@ -0,0 +1,16 @@
+// Counting per category
+// Each thread is responsible of one category per split
+__global__ void set_impurity1(unsigned int categories[$n],
+                              unsigned int count[$c]) {
+
+    int tid;        // Thread id
+    int i;          // Index variable
+
+    tid = blockIdx.x * blockDim.x + threadIdx.x; // Indexes the categories
+
+    if(tid < $c) {
+        for(i = 0 ; i < $n ; i++)
+            if(categories[i] == tid)
+                count[tid] += 1;
+    }
+}
