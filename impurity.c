d7e6667a94e031fcc87c5af4e78f12f5714bb2a1
Jean-Rémy Bancel <bancel@seas.harvard.edu>
2012-12-09 23:56:54 -0500

    Adding specific function for univariate impurity computation

--- impurity.c
+++ impurity.c
@@ -33,13 +33,12 @@ __global__ void impurity1(double samples[$n][$d],
     }
 }
 
-// Counting per category
+// Counting per category per side
 // Each thread is responsible of one category per split
 __global__ void impurity2(unsigned int categories[$n],
                           unsigned int count[$s][$c][2],
                           unsigned int Tl[$s],
-//                          unsigned int Tr[$s],
-                          unsigned position[$s][$n]) {
+                          unsigned int position[$s][$n]) {
 
     int t_x, t_y;   // Thread id
     int i;          // Index variable
@@ -56,8 +55,10 @@ __global__ void impurity2(unsigned int categories[$n],
             if(categories[i] == t_x) {
                 if(position[t_y][i] == 0)
                     count[t_y][t_x][0] += 1;
-                else
-                    count[t_y][t_x][1] += 1;
+                else {
+                    if(position[t_y][i] == 1)
+                        count[t_y][t_x][1] += 1;
+                }
             }
     }
 
@@ -68,13 +69,6 @@ __global__ void impurity2(unsigned int categories[$n],
             if(position[t_y][i] == 0)
                 Tl[t_y] += 1;
     }
-//    // Thread counting samples below the hyperplan
-//    if(t_x == $c + 1 && t_y < $s) {
-//        Tr[t_y] = 0;
-//        for(i = 0 ; i < $n ; i++)
-//            if(position[t_y][i] == 1)
-//                Tr[t_y] += 1;
-//    }
 }
 
 // TODO: implement it as a reduction
@@ -90,10 +84,42 @@ __global__ void impurity3(unsigned int count[$s][$c][2],
     tid = blockIdx.x * blockDim.x + threadIdx.x; // Indexes the splits
 
     if(tid < $s) {
-        for(i = 1 ; i < $c ; i++) {
-            GiniL -= (count[tid][i][0] * count[tid][i][0])/(double)((Tl[tid])*(Tl[tid]));
-            GiniR -= (count[tid][i][1] * count[tid][i][1])/(double)(($n-Tl[tid])*($n-Tl[tid]));
+        if(count[tid][0][0] == 0 && count[tid][0][1] == 0) {
+            impurity[tid] = 10;
         }
-        impurity[tid] = (Tl[tid] * GiniL + ($n-Tl[tid]) * GiniR)/((float)$n);
+        else {
+            for(i = 1 ; i < $c ; i++) {
+                GiniL -= (count[tid][i][0] * count[tid][i][0])/(double)((Tl[tid])*(Tl[tid]));
+                GiniR -= (count[tid][i][1] * count[tid][i][1])/(double)(($n-Tl[tid])*($n-Tl[tid]));
+            }
+            impurity[tid] = (Tl[tid] * GiniL + ($n-Tl[tid]) * GiniR)/((float)$n);
+        }
     }
 }
+
+// First step of impurity computation in one dimension
+__global__ void impurity4(double U[$n],
+                          double splits[$n][2],
+                          unsigned int position[$n][$n]) {
+
+    int t_x, t_y;   // Thread id
+
+    t_x = blockIdx.x * blockDim.x + threadIdx.x; // Indexes the samples
+    t_y = blockIdx.y * blockDim.y + threadIdx.y; // Indexes the splits
+
+    // Check boundaries
+    if(t_x < $n && t_y < $s) {
+        // Check if the hyperplan is valid
+        if(splits[t_y][1] > 0) {
+            if(U[t_x] < splits[t_y][0])
+                position[t_y][t_x] = 0;
+            else
+                position[t_y][t_x] = 1;
+        }
+        else {
+            position[t_y][t_x] = 2;
+        }
+    }
+}
+
+
